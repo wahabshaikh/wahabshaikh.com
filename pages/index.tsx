cf0789789c31ee0b1de132f1ad21af8dbb798c87
Wahab Shaikh <wahab.shaikh@somaiya.edu>
2021-09-02 13:40:45 +0530

    🚀 Display project cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
+import AboutMe from "../components/AboutMe";
 import ContactForm from "../components/ContactForm";
 import Container from "../components/Container";
 import Intro from "../components/Intro";
+import ProjectCard from "../components/ProjectCard";
 import Tagline from "../components/Tagline";
 
 const Home: React.FC = () => {
@@ -29,6 +31,26 @@ const Home: React.FC = () => {
       <section id="work" className="section">
         <Container>
           <h2 className="section-title">Work</h2>
+          <p className="section-subtitle">
+            I have helped build these (awesome) products!
+          </p>
+          <div className="grid sm:grid-flow-col gap-4 place-items-center">
+            <ProjectCard
+              title="Noggin"
+              logo="/noggin.png"
+              websiteUrl="https://noggin.so"
+              description="Noggin is the fastest and easiest Notion course builder. Quickly turn
+              your expertise into bite-sized, dynamic courses that grow with your
+              learners’ needs. Collect payments, use your domain and more— without a
+              single-line of code."
+            />
+            <ProjectCard
+              title="Social Snippet"
+              logo="/social-snippet.png"
+              websiteUrl="https://social-snippet.vercel.app"
+              description="Social Snippet turns your audio podcasts into socially sharable video clips with the transcription."
+            />
+          </div>
         </Container>
       </section>
 
@@ -36,9 +58,7 @@ const Home: React.FC = () => {
         <Container>
           <div className="flex flex-col items-center justify-center">
             <h2 className="section-title">Contact</h2>
-            <p className="mb-8 text-center sm:text-lg text-gray-700 dark:text-gray-300">
-              Interested? Send me a message.
-            </p>
+            <p className="section-subtitle">Interested? Send me a message.</p>
             <ContactForm />
           </div>
         </Container>
