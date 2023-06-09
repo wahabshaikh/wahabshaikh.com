25ddae02650633c20c7977b2137e6ca145c64303
Wahab Shaikh <hey.wahabshaikh@gmail.com>
2023-06-09 08:25:51 +0530

    feat: "Ah shit, here we go again.", again

--- pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import type { AppProps } from "next/app";
-
-import "../styles/globals.css";
-
-export default function CustomApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
